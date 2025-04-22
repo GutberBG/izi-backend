27a5ce59e922205019b9f33fd6c0aed191584040
GutberBG <gutber.blanco.gomez@gmail.com>
2025-04-21 21:02:11 -0400

    Base structure

---
+++ server.js
@@ -0,0 +1,15 @@
+const app = require('./app');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    console.log('Conectado a MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Servidor escuchando en el puerto ${PORT}`);
+    });
+  })
+  .catch(err => console.error('Error al conectar a MongoDB:', err));
